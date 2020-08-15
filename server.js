13543af430b14b4b132ab2d451803d8cc495fa91
Gaith-M <mhdgaith94@gmail.com>
2020-08-15 13:52:31 +0300

    project setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => res.send("Recieved"));
+
+app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
